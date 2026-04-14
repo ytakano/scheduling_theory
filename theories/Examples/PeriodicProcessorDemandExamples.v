b59c89dd55abaabe8c892d4b3c9a605cd070b425
Yuuki Takano <ytakanoster@gmail.com>
2026-04-14 20:51:18 +0000

    Replace periodic EDF window-DBF bridge with no-miss wrappers

--- theories/Examples/PeriodicProcessorDemandExamples.v
+++ theories/Examples/PeriodicProcessorDemandExamples.v
@@ -3,10 +3,12 @@ From RocqSched Require Import Foundation.Base.
 From RocqSched Require Import Abstractions.Scheduler.Interface.
 From RocqSched Require Import Abstractions.SchedulingAlgorithm.EnumCandidates.
 From RocqSched Require Import Uniprocessor.Policies.EDF.
+From RocqSched Require Import Analysis.Uniprocessor.BusyWindowSearch.
 From RocqSched Require Import TaskModels.Periodic.PeriodicFiniteHorizon.
 From RocqSched Require Import TaskModels.Periodic.PeriodicEDFBridge.
 From RocqSched Require Import TaskModels.Periodic.PeriodicEnumeration.
 From RocqSched Require Import TaskModels.Periodic.PeriodicWindowDemandBound.
+From RocqSched Require Import Semantics.Schedule.
 From RocqSched Require Import Examples.PeriodicExamples.
 
 Import ListNotations.
@@ -48,16 +50,28 @@ Proof.
     + constructor.
 Qed.
 
-Theorem periodic_example_edf_schedulable_by_window_dbf_auto :
-  schedulable_by_on
-    (periodic_jobset_upto T_ex tasks_ex offset_ex jobs_ex H_ex)
-    (edf_scheduler
-       (enum_candidates_of
-          (enum_periodic_jobs_upto T_ex tasks_ex offset_ex jobs_ex H_ex enumT_ex codec_ex)))
-    jobs_ex 1.
+Theorem periodic_example_edf_no_deadline_miss_by_window_dbf_auto :
+  forall sched j t1 t2,
+    scheduler_rel
+      (edf_scheduler
+         (enum_candidates_of
+            (enum_periodic_jobs_upto T_ex tasks_ex offset_ex jobs_ex H_ex enumT_ex codec_ex)))
+      jobs_ex 1 sched ->
+    periodic_jobset_upto T_ex tasks_ex offset_ex jobs_ex H_ex j ->
+    busy_window_witness sched (job_abs_deadline (jobs_ex j)) t1 t2 ->
+    t1 <= job_release (jobs_ex j) ->
+    job_abs_deadline (jobs_ex j) <= H_ex ->
+    (forall t j_run,
+      job_release (jobs_ex j) <= t < job_abs_deadline (jobs_ex j) ->
+      sched t 0 = Some j_run ->
+      periodic_jobset_deadline_between T_ex tasks_ex offset_ex jobs_ex
+        t1 (job_abs_deadline (jobs_ex j)) j_run ->
+      job_release (jobs_ex j) <= job_release (jobs_ex j_run)) ->
+    ~ missed_deadline jobs_ex 1 sched j.
 Proof.
-  apply periodic_edf_schedulable_by_window_dbf_on_finite_horizon_auto
-    with (enumT := enumT_ex).
+  intros sched j t1 t2 Hsched Hj Hwit Ht1rel Hj_H Hcarry_free.
+  eapply periodic_edf_no_deadline_miss_from_window_dbf_on_finite_horizon_auto
+    with (enumT := enumT_ex); eauto.
   - exact tasks_ex_well_formed.
   - exact enumT_ex_nodup.
   - intros τ Hτ.
--- theories/TaskModels/Periodic/PeriodicEDFBridge.v
+++ theories/TaskModels/Periodic/PeriodicEDFBridge.v
@@ -12,6 +12,7 @@ From RocqSched Require Import TaskModels.Periodic.PeriodicFiniteHorizon.
 From RocqSched Require Import TaskModels.Periodic.PeriodicEnumeration.
 From RocqSched Require Import TaskModels.Periodic.PeriodicFiniteOptimalityLift.
 From RocqSched Require Import TaskModels.Periodic.PeriodicWindowDemandBound.
+From RocqSched Require Import Analysis.Uniprocessor.BusyWindowSearch.
 From RocqSched Require Import Analysis.Uniprocessor.EDFProcessorDemand.
 Import ListNotations.
 
@@ -56,65 +57,156 @@ Proof.
     T tasks offset H enumT jobs codec Hwf HenumT_complete HenumT_sound Hfeas).
 Qed.
 
-Theorem periodic_edf_schedulable_by_window_dbf_on_finite_horizon :
-  forall T T_bool tasks offset H enumT enumJ jobs,
-    well_formed_periodic_tasks_on T tasks ->
-    NoDup enumT ->
-    (forall τ, T_bool τ = true <-> T τ) ->
-    (forall j, periodic_jobset_upto T tasks offset jobs H j -> In j enumJ) ->
-    (forall j, In j enumJ -> periodic_jobset_upto T tasks offset jobs H j) ->
-    (forall t1 t2,
-      t1 <= t2 ->
-      t2 <= H ->
-      taskset_periodic_dbf_window tasks offset enumT t1 t2 <= t2 - t1) ->
-    schedulable_by_on
-      (periodic_jobset_upto T tasks offset jobs H)
-      (edf_scheduler (enum_candidates_of enumJ))
-      jobs 1.
-Proof.
-  intros T T_bool tasks offset H enumT enumJ jobs
-         Hwf HnodupT HTbool Henum_complete Henum_sound Hdbf.
-  apply periodic_edf_optimality_on_finite_horizon
-    with (T_bool := T_bool) (enumJ := enumJ).
-  - exact HTbool.
-  - exact Henum_complete.
-  - exact Henum_sound.
-  - exact (periodic_window_dbf_implies_edf_feasible_on_finite_horizon
-             T tasks offset H enumT enumJ jobs
-             Hwf HnodupT Henum_complete Henum_sound Hdbf).
-Qed.
-
-Theorem periodic_edf_schedulable_by_window_dbf_on_finite_horizon_auto :
-  forall T tasks offset H enumT jobs
-         (codec : PeriodicFiniteHorizonCodec T tasks offset jobs H),
+Theorem periodic_edf_no_deadline_miss_from_window_dbf_on_finite_horizon :
+  forall T tasks offset H enumT enumJ jobs
+         (codec : PeriodicFiniteHorizonCodec T tasks offset jobs H)
+         sched j t1 t2,
     well_formed_periodic_tasks_on T tasks ->
     NoDup enumT ->
     (forall τ, T τ -> In τ enumT) ->
     (forall τ, In τ enumT -> T τ) ->
+    (forall x, periodic_jobset_upto T tasks offset jobs H x -> In x enumJ) ->
+    (forall x, In x enumJ -> periodic_jobset_upto T tasks offset jobs H x) ->
+    scheduler_rel (edf_scheduler (enum_candidates_of enumJ)) jobs 1 sched ->
+    periodic_jobset_upto T tasks offset jobs H j ->
+    busy_window_witness sched (job_abs_deadline (jobs j)) t1 t2 ->
+    t1 <= job_release (jobs j) ->
+    job_abs_deadline (jobs j) <= H ->
+    (forall t j_run,
+      job_release (jobs j) <= t < job_abs_deadline (jobs j) ->
+      sched t 0 = Some j_run ->
+      periodic_jobset_deadline_between T tasks offset jobs
+        t1 (job_abs_deadline (jobs j)) j_run ->
+      job_release (jobs j) <= job_release (jobs j_run)) ->
+    (forall t1' t2',
+      t1' <= t2' ->
+      t2' <= H ->
+      taskset_periodic_dbf_window tasks offset enumT t1' t2' <= t2' - t1') ->
+    ~ missed_deadline jobs 1 sched j.
+Proof.
+  intros T tasks offset H enumT enumJ jobs codec sched j t1 t2
+         Hwf HnodupT HenumT_complete HenumT_sound
+         HenumJ_complete HenumJ_sound Hsched Hj Hwit Ht1rel Hj_H
+         Hcarry_free Hdbf.
+  eapply
+    periodic_window_dbf_implies_no_deadline_miss_under_edf_if_covering_busy_window_and_no_carry_in;
+    eauto.
+Qed.
+
+Theorem periodic_edf_no_deadline_miss_from_window_dbf_on_finite_horizon_auto :
+  forall T tasks offset H enumT jobs
+         (codec : PeriodicFiniteHorizonCodec T tasks offset jobs H)
+         sched j t1 t2,
+    well_formed_periodic_tasks_on T tasks ->
+    NoDup enumT ->
+    (forall τ, T τ -> In τ enumT) ->
+    (forall τ, In τ enumT -> T τ) ->
+    scheduler_rel
+      (edf_scheduler
+         (enum_candidates_of
+            (enum_periodic_jobs_upto T tasks offset jobs H enumT codec)))
+      jobs 1 sched ->
+    periodic_jobset_upto T tasks offset jobs H j ->
+    busy_window_witness sched (job_abs_deadline (jobs j)) t1 t2 ->
+    t1 <= job_release (jobs j) ->
+    job_abs_deadline (jobs j) <= H ->
+    (forall t j_run,
+      job_release (jobs j) <= t < job_abs_deadline (jobs j) ->
+      sched t 0 = Some j_run ->
+      periodic_jobset_deadline_between T tasks offset jobs
+        t1 (job_abs_deadline (jobs j)) j_run ->
+      job_release (jobs j) <= job_release (jobs j_run)) ->
+    (forall t1' t2',
+      t1' <= t2' ->
+      t2' <= H ->
+      taskset_periodic_dbf_window tasks offset enumT t1' t2' <= t2' - t1') ->
+    ~ missed_deadline jobs 1 sched j.
+Proof.
+  intros T tasks offset H enumT jobs codec sched j t1 t2
+         Hwf HnodupT HenumT_complete HenumT_sound
+         Hsched Hj Hwit Ht1rel Hj_H Hcarry_free Hdbf.
+  eapply periodic_edf_no_deadline_miss_from_window_dbf_on_finite_horizon
+    with (codec := codec)
+         (enumJ := enum_periodic_jobs_upto T tasks offset jobs H enumT codec);
+    eauto using enum_periodic_jobs_upto_complete, enum_periodic_jobs_upto_sound.
+Qed.
+
+Theorem periodic_edf_feasible_schedule_from_window_dbf_on_finite_horizon :
+  forall T tasks offset H enumT enumJ jobs
+         (codec : PeriodicFiniteHorizonCodec T tasks offset jobs H)
+         sched,
+    well_formed_periodic_tasks_on T tasks ->
+    NoDup enumT ->
+    (forall τ, T τ -> In τ enumT) ->
+    (forall τ, In τ enumT -> T τ) ->
+    (forall x, periodic_jobset_upto T tasks offset jobs H x -> In x enumJ) ->
+    (forall x, In x enumJ -> periodic_jobset_upto T tasks offset jobs H x) ->
+    scheduler_rel (edf_scheduler (enum_candidates_of enumJ)) jobs 1 sched ->
+    (forall j,
+      periodic_jobset_upto T tasks offset jobs H j ->
+      job_abs_deadline (jobs j) <= H /\
+      exists t1 t2,
+        busy_window_witness sched (job_abs_deadline (jobs j)) t1 t2 /\
+        t1 <= job_release (jobs j) /\
+        (forall t j_run,
+          job_release (jobs j) <= t < job_abs_deadline (jobs j) ->
+          sched t 0 = Some j_run ->
+          periodic_jobset_deadline_between T tasks offset jobs
+            t1 (job_abs_deadline (jobs j)) j_run ->
+          job_release (jobs j) <= job_release (jobs j_run))) ->
     (forall t1 t2,
       t1 <= t2 ->
       t2 <= H ->
       taskset_periodic_dbf_window tasks offset enumT t1 t2 <= t2 - t1) ->
-    schedulable_by_on
-      (periodic_jobset_upto T tasks offset jobs H)
+    feasible_schedule_on (periodic_jobset_upto T tasks offset jobs H) jobs 1 sched.
+Proof.
+  intros T tasks offset H enumT enumJ jobs codec sched
+         Hwf HnodupT HenumT_complete HenumT_sound
+         HenumJ_complete HenumJ_sound Hsched
+         Hjob_bridge Hdbf.
+  unfold feasible_schedule_on.
+  intros j Hj.
+  destruct (Hjob_bridge j Hj) as
+      [Hj_H [t1 [t2 [Hwit [Ht1rel Hcarry_free]]]]].
+  eapply periodic_edf_no_deadline_miss_from_window_dbf_on_finite_horizon; eauto.
+Qed.
+
+Theorem periodic_edf_feasible_schedule_from_window_dbf_on_finite_horizon_auto :
+  forall T tasks offset H enumT jobs
+         (codec : PeriodicFiniteHorizonCodec T tasks offset jobs H)
+         sched,
+    well_formed_periodic_tasks_on T tasks ->
+    NoDup enumT ->
+    (forall τ, T τ -> In τ enumT) ->
+    (forall τ, In τ enumT -> T τ) ->
+    scheduler_rel
       (edf_scheduler
          (enum_candidates_of
             (enum_periodic_jobs_upto T tasks offset jobs H enumT codec)))
-      jobs 1.
+      jobs 1 sched ->
+    (forall j,
+      periodic_jobset_upto T tasks offset jobs H j ->
+      job_abs_deadline (jobs j) <= H /\
+      exists t1 t2,
+        busy_window_witness sched (job_abs_deadline (jobs j)) t1 t2 /\
+        t1 <= job_release (jobs j) /\
+        (forall t j_run,
+          job_release (jobs j) <= t < job_abs_deadline (jobs j) ->
+          sched t 0 = Some j_run ->
+          periodic_jobset_deadline_between T tasks offset jobs
+            t1 (job_abs_deadline (jobs j)) j_run ->
+          job_release (jobs j) <= job_release (jobs j_run))) ->
+    (forall t1 t2,
+      t1 <= t2 ->
+      t2 <= H ->
+      taskset_periodic_dbf_window tasks offset enumT t1 t2 <= t2 - t1) ->
+    feasible_schedule_on (periodic_jobset_upto T tasks offset jobs H) jobs 1 sched.
 Proof.
-  intros T tasks offset H enumT jobs codec
-         Hwf HnodupT HenumT_complete HenumT_sound Hdbf.
-  apply periodic_edf_optimality_on_finite_horizon_auto.
-  - exact Hwf.
-  - exact HenumT_complete.
-  - exact HenumT_sound.
-  - exact (periodic_window_dbf_implies_edf_feasible_on_finite_horizon
-             T tasks offset H enumT
-             (enum_periodic_jobs_upto T tasks offset jobs H enumT codec)
-             jobs
-             Hwf
-             HnodupT
-             (enum_periodic_jobs_upto_complete T tasks offset jobs H enumT codec Hwf HenumT_complete)
-             (enum_periodic_jobs_upto_sound T tasks offset jobs H enumT codec HenumT_sound)
-             Hdbf).
+  intros T tasks offset H enumT jobs codec sched
+         Hwf HnodupT HenumT_complete HenumT_sound
+         Hsched Hjob_bridge Hdbf.
+  eapply periodic_edf_feasible_schedule_from_window_dbf_on_finite_horizon
+    with (codec := codec)
+         (enumJ := enum_periodic_jobs_upto T tasks offset jobs H enumT codec);
+    eauto using enum_periodic_jobs_upto_complete, enum_periodic_jobs_upto_sound.
 Qed.
