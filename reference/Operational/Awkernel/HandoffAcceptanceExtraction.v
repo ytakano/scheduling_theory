80ae93502824c43756a456fc81858d5921386777
Yuuki Takano <ytakanoster@gmail.com>
2026-04-24 03:50:37 +0900

    remove references
    Signed-off-by: Yuuki Takano <ytakanoster@gmail.com>

--- reference/Operational/Awkernel/HandoffAcceptanceExtraction.v
+++
@@ -1,8 +0,0 @@
-From Stdlib Require Extraction.
-From RocqSched Require Import Operational.Awkernel.Minimal.CapturedTraceSyntax.
-From RocqSched Require Import Operational.Awkernel.HandoffTraceFamily.
-
-Extraction Language Haskell.
-
-Extraction "/scheduling_theory/extracted/haskell/AwkernelHandoffAcceptance.hs"
-  awk_handoff_accepts_rows.
